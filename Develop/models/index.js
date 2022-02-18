f659b463e22be5013200ff0216b82ce0f6f4ddeb
Ben Lu <yudonglv@gmail.com>
2022-02-17 23:28:55 -0800

    add async for the await function

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -19,14 +19,18 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_id',
-});
+  through: {
+    model: ProductTag
+  },
+})
 
 // Tags belongToMany Products (through ProductTag)
 
 Tag.belongsToMany(Product, {
-  foreignKey: 'category_id',
-});
+  through: {
+    model: ProductTag
+  },
+})
 
 module.exports = {
   Product,
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -3,7 +3,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // The `/api/products` endpoint
 // get all products
-router.get('/', (req, res) => {
+router.get('/',async (req, res) => {
   // find all products
   // Included its associated Category and Tag data
   try {
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id',async (req, res) => {
   // find a single product by its `id`
   // Included its associated Category and Tag data
   try {
@@ -107,7 +107,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const userData = await Product.delete(req.body, {
