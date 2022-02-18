38df31f298d78c17903ac11acc429753595be23c
Ben Lu <yudonglv@gmail.com>
2022-02-17 22:34:32 -0800

    added index.js for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  // TODO: Add a comment describing the functionality of this property
+  onDelete: 'CASCADE',
+});
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  foreignKey: 'category_id',
+});
 
 module.exports = {
   Product,
